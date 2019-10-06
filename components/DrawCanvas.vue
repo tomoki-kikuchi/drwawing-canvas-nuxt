5530cd430367c2a257872741a93d0c9361cb8acd
tomoki <www.tomoki.jp@gmail.com>
2019-10-06 16:18:00 +0900

    リファクタリング

--- components/DrawCanvas.vue
+++ components/DrawCanvas.vue
@@ -1,125 +1,29 @@
 <template>
   <div class="canvasContainer">
-    <canvas id="canvas" class="canvas" width="600" height="600"></canvas>
-    <div class="toolContainer">
-      <div class="tool">
-        <div class="buttonContainer">
-          <div class="buttonInner">
-            <Button type="default" icon="undo" @click="undo" />
-            <Button type="default" icon="redo" @click="redo" />
-            <Button
-              icon="edit"
-              :type="isDrawingMode ? 'primary' : 'default'"
-              @click="toggleDrawingMode"
-            ></Button>
-            <Button
-              icon="search"
-              :type="isStampMode ? 'primary' : 'default'"
-              @click="toggleStampMode"
-            />
-
-            <Button type="primary" icon="download" @click="saveImage"
-              >Download</Button
-            >
-          </div>
-          <Button type="danger" icon="delete" @click="deleteCanvas" />
-        </div>
-      </div>
-
-      <transition name="tools">
-        <div v-show="isDrawingMode">
-          <div class="tool">
-            <p class="toolLabel">線の太さ</p>
-            <Slider id="test" v-model="brushWidth" :min="1" :max="20" />
-          </div>
-          <div class="tool">
-            <p class="toolLabel">線の色</p>
-            <client-only>
-              <compact-picker v-model="strokeColor" />
-            </client-only>
-          </div>
-        </div>
-      </transition>
-      <transition name="tools">
-        <div v-show="isStampMode">
-          <div class="tool">
-            <p class="toolLabel">スタンプ</p>
-            <Avatar
-              v-for="(stamp, index) in stampList"
-              :key="index"
-              class="stamp"
-              size="large"
-              :src="stamp"
-              :class="{ isSelected: selectStampIndex === index }"
-              @click="selectStamp(index)"
-            />
-          </div>
-        </div>
-      </transition>
-    </div>
+    <canvas :id="id" class="canvas" :width="width" :height="height"></canvas>
   </div>
 </template>
 
 <script>
-import { Avatar, Button, Slider } from 'ant-design-vue'
-import { Compact } from 'vue-color'
 import DrawableCanvas from '~/plugins/DrawableCanvas'
-
-const stampList = [
-  '/images/stamp/animal_buta.png',
-  '/images/stamp/animal_kuma.png',
-  '/images/stamp/animal_mitsubachi.png',
-  '/images/stamp/animal_neko.png',
-  '/images/stamp/animal_penguin.png',
-  '/images/stamp/shinwa_hades.png'
-]
 
 export default {
   name: 'DrawCanvas',
-  components: {
-    Avatar,
-    Button,
-    // Checkbox,
-    Slider,
-    'compact-picker': Compact
+  components: {},
+  props: {
+    width: { type: Number, default: 600 },
+    height: { type: Number, default: 600 },
+    id: { type: String, required: true }
   },
   data() {
     return {
-      canvas: null,
-      brushWidth: 1,
-      isDrawingMode: false,
-      isStampMode: false,
-      strokeColor: 'red',
-      selectStampIndex: null
+      canvas: null
     }
   },
-  computed: {
-    stampList() {
-      return stampList
-    }
-  },
-  watch: {
-    brushWidth(val) {
-      this.setBrushWidth(val)
-    },
-    strokeColor(color) {
-      this.setStrokeColor(color.hex)
-    },
-    isDrawingMode(state, oldState) {
-      if (state) {
-        this.isStampMode = false
-      }
-      this.canvas.drawingMode = state
-    },
-    isStampMode(state, oldState) {
-      if (state) {
-        this.isDrawingMode = false
-        this.canvas.drawingMode = false
-      }
-    }
-  },
+  computed: {},
+
   mounted() {
-    this.canvas = new DrawableCanvas('canvas')
+    this.canvas = new DrawableCanvas(this.id)
   },
   methods: {
     undo() {
@@ -128,7 +32,7 @@ export default {
     redo() {
       this.canvas.redo()
     },
-    deleteCanvas() {
+    deleteAll() {
       this.canvas.deleteAll()
     },
     setBrushWidth(width) {
@@ -137,27 +41,14 @@ export default {
     setStrokeColor(color) {
       this.canvas.setStrokeColor(color)
     },
-    setStamp(imagePath) {
-      this.canvas.setStamp(imagePath)
+    setDrawingMode(flag) {
+      this.canvas.drawingMode = flag
     },
     setStampImage(imagePath) {
       this.canvas.setStampImage(imagePath)
     },
-    selectStamp(index) {
-      this.selectStampIndex = index
-      this.canvas.setStampImage(this.stampList[index])
-    },
-
     saveImage() {
       this.canvas.saveImage()
-    },
-    toggleDrawingMode() {
-      this.isDrawingMode = !this.isDrawingMode
-      // this.isStampMode = false
-    },
-    toggleStampMode() {
-      // this.isDrawingMode = false
-      this.isStampMode = !this.isStampMode
     }
   }
 }
@@ -172,39 +63,5 @@ export default {
 .canvas {
   border: solid #494949 1px;
   max-width: 600px;
-}
-
-.toolContainer {
-  margin-top: 50px;
-  .tool {
-    margin-top: 10px;
-    .toolLabel {
-      margin-bottom: 10px;
-    }
-  }
-}
-
-.buttonContainer {
-  display: flex;
-  justify-content: space-between;
-  .buttonInner {
-  }
-}
-
-.stamp {
-  opacity: 1;
-  &.isSelected {
-    opacity: 1;
-    border: solid 3px #ff0000;
-  }
-}
-
-.tools-enter-active,
-.tools-leave-active {
-  transition: opacity 0.5s;
-}
-.tools-enter,
-.tools-leave-to {
-  opacity: 0;
 }
 </style>
---
+++ components/StampSelect.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <p class="toolLabel">スタンプ</p>
+    <Avatar
+      v-for="(stamp, index) in stampList"
+      :key="index"
+      class="stamp"
+      size="large"
+      :src="stamp"
+      :class="{ isSelected: selectStampIndex === index }"
+      @click="selectStamp(index)"
+    />
+  </div>
+</template>
+
+<script>
+import { Avatar } from 'ant-design-vue'
+const stampList = [
+  '/images/stamp/animal_buta.png',
+  '/images/stamp/animal_kuma.png',
+  '/images/stamp/animal_mitsubachi.png',
+  '/images/stamp/animal_neko.png',
+  '/images/stamp/animal_penguin.png',
+  '/images/stamp/shinwa_hades.png'
+]
+
+export default {
+  name: 'StampSelect',
+  components: {
+    Avatar
+  },
+  data() {
+    return {
+      selectStampIndex: null
+    }
+  },
+  computed: {
+    stampList() {
+      return stampList
+    }
+  },
+  methods: {
+    selectStamp(index) {
+      console.log('selectStamp', index)
+      this.selectStampIndex = index
+      this.$emit('selectImage', this.stampList[index])
+      // this.canvas.setStampImage(this.stampList[index])
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.stamp {
+  opacity: 1;
+  &.isSelected {
+    opacity: 1;
+    border: solid 3px #ff0000;
+  }
+}
+</style>
